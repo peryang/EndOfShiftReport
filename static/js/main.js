6c1781218a55d5b6a65c30fdbe9714669260b488
bjyangxiuwu <bjyangxiuwu@corp.netease.com>
2017-07-19 10:29:01 +0800

    提交资源信息

--- static/js/main.js
+++ static/js/main.js
@@ -255,8 +255,19 @@ $(function(){
 	$('#submit').click(function(ev) {
 		ev.stopPropagation();
 		ev.preventDefault();
+		$('#submit').addClass("hide");
+		$(".column-add").addClass("hide");
+		$(".column-del").addClass("hide");
+		if($(".editable-empty").length > 0){
+			$("#enable").click();
+		}
+		$("#enable").addClass("hide");
 		html2canvas(document.getElementById("main"), {
 			onrendered: function(canvas) {
+				$("#enable").removeClass("hide");
+				$('#submit').removeClass("hide");
+				$(".column-add").removeClass("hide");
+				$(".column-del").removeClass("hide");
 				var image64 = canvas.toDataURL("image/png");
 				var postData = getPostData();
 				if(initData != postData.data){
